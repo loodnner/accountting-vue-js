7a759f9b9792647354311117562f7ea1d17b5118
loodnn <loudjane@163.com>
2021-07-12 10:45:51 +0800

    deploy

---
+++ precache-manifest.522e19de4579fda3739e94747ff1579e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717ca8c84550f7a984f7",
+    "url": "/accountting-vue-js/css/app.e3786bc5.css"
+  },
+  {
+    "revision": "81b8e33fbc3e9e4642a4e4f77c0a53d2",
+    "url": "/accountting-vue-js/index.html"
+  },
+  {
+    "revision": "717ca8c84550f7a984f7",
+    "url": "/accountting-vue-js/js/app.80360be2.js"
+  },
+  {
+    "revision": "285e1cf81f5aca89a935",
+    "url": "/accountting-vue-js/js/chunk-vendors.62336e55.js"
+  },
+  {
+    "revision": "8c037bf97bc8532092c051e1d550146a",
+    "url": "/accountting-vue-js/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/accountting-vue-js/robots.txt"
+  }
+]);
